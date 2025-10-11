2b95900293df6b3c42828f59e2646b4ed21ea94e
Eishin <erickjcha@gmail.com>
2025-10-11 10:21:18 -0500

    sadx

---
+++ semana11/dia01-01-react-custom-hooks-ejm/src/App.jsx
@@ -0,0 +1,16 @@
+import Counter from "./Components/Counter"
+import ProductList from "./Components/ProductList"
+
+const App = () => {
+  return (
+    <main>
+      <h1>Custom hooks</h1>
+
+      <Counter />
+
+      <ProductList />
+    </main>
+  )
+}
+
+export default App
